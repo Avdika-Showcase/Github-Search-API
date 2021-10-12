a480442ee61e247985901befe40e03e37a6e8f3f
Avdika <avdukins@gmail.com>
2021-10-13 00:09:19 +0200

    clone from old test assignment project

---
+++ src/Components/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Text } from '../Styles/NotFoundStyle';
+
+function Notfound() {
+  return (
+    <div>
+      <Text>Sorry, there is no results for your request. Try another one.</Text>
+    </div>
+  )
+}
+
+export default Notfound;
---
+++ src/Components/Search.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import axios from 'axios';
+import { trackPromise } from 'react-promise-tracker';
+
+import { Container } from '../Styles/SearchStyle';
+import SearchForm from './SearchForm';
+import Repos from './Repositories';
+import Spinner from './Spinner';
+import NotFound from './NotFound';
+
+function Search() {
+  const [repos, setRepos] = React.useState([]);
+  const [emptySearch, setEmptySearch] = React.useState(false);
+
+  const handleResult = result => {
+    setRepos(result);
+    setEmptySearch(result.length === 0);
+  };
+
+  const searchRepos = (keyword) => {
+    setRepos([]);
+
+    trackPromise(
+      axios
+        .get(`https://api.github.com/search/repositories?q=${keyword}+in%3Aname&sort=stars&order=desc`)
+        .then(result => handleResult(result.data.items))
+        .catch((error) => {
+          console.log('we have received an error: ', error);
+        }));
+  };
+
+  return (
+    <Container>
+      <SearchForm
+        placeholder="Enter Repository Name"
+        buttonText="Search"
+        onSubmit={value => searchRepos(value)}
+      />
+      { emptySearch && (<NotFound />) }
+      <Spinner />
+      <Repos items={repos} />
+    </Container>
+  )
+}
+
+export default Search;
